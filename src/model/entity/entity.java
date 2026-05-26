9b8e1aec08a1d6960084e6bc0c03fc4191119e57
Samuel TI - Yudo SA <informatica@yudosa.com.br>
2026-05-26 16:09:38 -0300

    feat: add core entity system

--- src/model/entity/entity.java
+++ src/model/entity/entity.java
@@ -1,4 +1,25 @@
 package model.entity;
 
-public class entity {
+public abstract class Entity {
+
+    private String name;
+    private int hp;
+    private int maxHp;
+
+    public Entity(String name, int maxHp) {
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp; // Começa com a vida cheia
+    }
+
+    public String getName() { return name; }
+    public int getHp() { return hp; }
+    public int getMaxHp() { return maxHp; }
+
+    public void takeDamage(int damage) {
+        this.hp -= damage;
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+    }
 }
--- src/model/entity/player/player.java
+++ src/model/entity/player/player.java
@@ -1,4 +1,23 @@
 package model.entity.player;
 
-public class player {
+import model.entity.Entity;
+
+public class Player extends Entity {
+
+    private int level;
+    private int exp;
+
+    public Player(String name) {
+        super(name, 100);
+        this.level = 1;
+        this.exp = 0;
+    }
+
+    public int getLevel() { return level; }
+    public int getExp() { return exp; }
+
+    public void gainExp(int amount) {
+        this.exp += amount;
+        System.out.println(getName() + " ganhou " + amount + " de experiência!");
+    }
 }
